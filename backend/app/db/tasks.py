1a0f89b3cd58b02413fde090abfdca00bdfba03a
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2022-03-26 20:22:42 -0300

    Adding get-cleaning-by-id endpoint + Tests for current POST and GET endpoints

--- backend/app/db/tasks.py
+++ backend/app/db/tasks.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from databases import Database
 from app.core.config import DATABASE_URL
@@ -6,7 +7,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 async def connect_to_db(app: FastAPI) -> None:
-    database = Database(DATABASE_URL, min_size=2, max_size=10)
+    DB_URL = f"{DATABASE_URL}_test" if os.environ.get("TESTING") else DATABASE_URL
+    database = Database(DB_URL, min_size=2, max_size=10)
 
     try:
         await database.connect()
---
+++ backend/tests/conftest.py
@@ -0,0 +1,58 @@
+import alembic
+import os
+import pytest
+import warnings
+from alembic.config import Config
+from asgi_lifespan import LifespanManager
+from databases import Database
+from fastapi import FastAPI
+from httpx import AsyncClient
+from app.models.cleaning import CleaningCreate, CleaningInDB
+from app.db.repositories.cleanings import CleaningsRepository
+
+
+# Apply migrations at beginning and end of testing session
+@pytest.fixture(scope="session")
+def apply_migrations():
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    os.environ["TESTING"] = "1"
+    config = Config("alembic.ini")
+
+    alembic.command.upgrade(config, "head")
+    yield
+    alembic.command.downgrade(config, "base")
+
+# Create a new application for testing
+@pytest.fixture
+def app(apply_migrations: None) -> FastAPI:
+    from app.api.server import get_application
+    
+    return  get_application()
+
+# Grab a reference to our database when needed
+@pytest.fixture
+def db(app: FastAPI) -> Database:
+    return app.state._db
+
+# Make requests in our tests
+@pytest.fixture
+async def client(app: FastAPI) -> AsyncClient:
+    async with LifespanManager(app):
+        async with AsyncClient(
+            app=app,
+            base_url="http://testserver",
+            headers={"Content-Type": "application/json"}
+        ) as client:
+            yield client
+
+@pytest.fixture
+async def test_cleaning(db: Database) -> CleaningInDB:
+    cleaning_repo = CleaningsRepository(db)
+    new_cleaning = CleaningCreate(
+        name="fake cleaning name",
+        description="fake cleaning description",
+        price=9.99,
+        cleaning_type="spot_clean",
+    )
+
+    return await cleaning_repo.create_cleaning(new_cleaning=new_cleaning)
